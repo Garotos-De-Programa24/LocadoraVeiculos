edd75ae2b34f0cb32b5492da1b51b926da354cfc
AnderSpidy <andersonreinaldo2347@gmail.com>
2022-06-22 00:57:53 -0300

    Add project files.

---
+++ LocadoraVeiculos.Dominio/ModuloCliente/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using LocadoraVeiculos.Dominio.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraVeiculos.Dominio.ModuloCliente
+{
+    public class Cliente : EntidadeBase<Cliente>
+    {
+        public override void Atualizar(Cliente Registro)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
