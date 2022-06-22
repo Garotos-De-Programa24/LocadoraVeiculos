29da0bda23098f651274340de80892a64655a9dc
nicolas <nicolas56mil@gmail.com>
2022-06-22 16:37:08 -0300

    atributosFuncionarioClientes

--- LocadoraVeiculos.Dominio/ModuloCliente/Cliente.cs
+++ LocadoraVeiculos.Dominio/ModuloCliente/Cliente.cs
@@ -9,7 +9,23 @@ namespace LocadoraVeiculos.Dominio.ModuloCliente
 {
     public class Cliente : EntidadeBase<Cliente>
     {
-        private string _nome; 
+        public Cliente(string nome, int cPFCNPJ, string endereco, int Cnh, string email, int telefone)
+        {
+            Nome = nome;
+            CPFCNPJ = cPFCNPJ;
+            Endereco = endereco;
+            CNH = Cnh;
+            Email = email;
+            Telefone = telefone;
+        } 
+
+        public string Nome { get; set; }
+        public int CPFCNPJ { get; set; }
+        public string Endereco { get; set; }
+        public int Telefone { get; set; }
+        public int CNH { get; set; }
+        public string Email { get; set; }
+
         public override void Atualizar(Cliente Registro)
         {
             throw new NotImplementedException();
--- LocadoraVeiculos.Dominio/ModuloFuncionario/Funcionario.cs
+++ LocadoraVeiculos.Dominio/ModuloFuncionario/Funcionario.cs
@@ -8,5 +8,25 @@ namespace LocadoraVeiculos.Dominio.ModuloFuncionario
 {
     internal class Funcionario
     {
+        public Funcionario(string nome, string login, string senha, decimal salario, DateTime admissao, bool gerente)
+        {
+
+            Nome = nome;
+            Login = login;
+            Senha = senha;
+            Salario = salario;
+            Admissao = admissao;
+            Gerente = gerente;
+        }
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public decimal Salario { get; set; }
+        public DateTime Admissao { get; set; }  
+        public bool Gerente { get; set; }   
+
+
+
+
     }
 }
