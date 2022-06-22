74debd4509e799813cb12cd96ad1fd2fb8a101b4
AnderSpidy <andersonreinaldo2347@gmail.com>
2022-06-22 19:33:46 -0300

    Adição do Cliente e Funcionario atributos feito pelo Nicolas

--- LocadoraVeiculos.Dominio/ModuloFuncionario/Funcionario.cs
+++ LocadoraVeiculos.Dominio/ModuloFuncionario/Funcionario.cs
@@ -9,6 +9,23 @@ namespace LocadoraVeiculos.Dominio.ModuloFuncionario
 {
     public class Funcionario : EntidadeBase<Funcionario>
     {
+        public Funcionario(string nome, string login, string senha, decimal salario, DateTime admissao, bool gerente)
+        {
+
+            Nome = nome;
+            Login = login;
+            Senha = senha;
+            Salario = salario;
+            Admissao = admissao;
+            Gerente = gerente;
+        }
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public decimal Salario { get; set; }
+        public DateTime Admissao { get; set; }
+        public bool Gerente { get; set; }
+
         public override void Atualizar(Funcionario Registro)
         {
             throw new NotImplementedException();
