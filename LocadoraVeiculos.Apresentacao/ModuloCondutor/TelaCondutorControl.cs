d03b121754dacd50528761bfcb51192ef266bcce
matheus nunes <matheusbnunes3@hotmail.com>
2022-06-30 16:56:30 -0300

    Alteraçoes

--- LocadoraVeiculos.Apresentacao/ModuloCondutor/TelaCondutorControl.cs
+++ LocadoraVeiculos.Apresentacao/ModuloCondutor/TelaCondutorControl.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LocadoraVeiculos.Apresentacao.Compartilhado;
+using LocadoraVeiculos.Dominio.ModuloCondutor;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,42 @@ namespace LocadoraVeiculos.Apresentacao.ModuloCondutor
         public TelaCondutorControl()
         {
             InitializeComponent();
+            grid.ConfigurarGridZebrado();
+            grid.ConfigurarGridSomenteLeitura();
+            grid.Columns.AddRange(ObterColunas());
+        }
+
+        public DataGridViewColumn[] ObterColunas()
+        {
+            var colunas = new DataGridViewColumn[]
+            {
+                new DataGridViewTextBoxColumn { DataPropertyName = "Id", HeaderText = "Id"},
+                new DataGridViewTextBoxColumn { DataPropertyName = "Cliente", HeaderText = "Cliente"},
+                new DataGridViewTextBoxColumn { DataPropertyName = "Nome", HeaderText = "Nome"},
+                new DataGridViewTextBoxColumn { DataPropertyName = "Cpf", HeaderText = "CPF"},
+                new DataGridViewTextBoxColumn { DataPropertyName = "Endereco", HeaderText = "Endereço"},
+                new DataGridViewTextBoxColumn { DataPropertyName = "Email", HeaderText = "E-Mail"},
+                new DataGridViewTextBoxColumn { DataPropertyName = "Telefone", HeaderText = "Telefone"},
+            };
+
+            return colunas;
+        }
+
+        public void AtualizarRegistros(List<Condutor> condutores)
+        {
+            grid.Rows.Clear();
+
+            foreach (var condutor in condutores)
+            {
+                grid.Rows.Add(condutor.Id, condutor.Cliente.Nome, condutor.Nome, condutor.Cpf, condutor.Endereco,
+                     condutor.Email, condutor.Telefone);
+
+            }
+        }
+
+        public int ObtemNumeroCondutorSelecionado()
+        {
+            return grid.SelecionarNumero<int>();
         }
     }
 }
