1096edbadd136d056a1f6d7824e4d850a13281cf
matheus nunes <matheusbnunes3@hotmail.com>
2022-07-12 16:41:20 -0300

    Alterações Banco de Dados

--- LocadoraVeiculos.Projeto.BancoDados/dbo/Tables/TBVeiculo.sql
+++ LocadoraVeiculos.Projeto.BancoDados/dbo/Tables/TBVeiculo.sql
@@ -1,4 +1,20 @@
 ﻿CREATE TABLE [dbo].[TBVeiculo] (
-    [Id] INT NOT NULL
+    [Id]               INT           IDENTITY (1, 1) NOT NULL,
+    [Foto]             VARCHAR (MAX) NULL,
+    [Marca]            VARCHAR (50)  NOT NULL,
+    [Ano]              VARCHAR (50)  NOT NULL,
+    [Placa]            VARCHAR (50)  NOT NULL,
+    [CapacidadeTanque] VARCHAR (50)  NOT NULL,
+    [KmPercorrido]     VARCHAR (50)  NOT NULL,
+    [Combustivel]      VARCHAR (50)  NOT NULL,
+    [Agrupamento_Id]   INT           NOT NULL,
+    [VeiculoNome]      VARCHAR (100) NOT NULL,
+    [Cor]              VARCHAR (50)  NOT NULL,
+    CONSTRAINT [PK_TBVeiculo] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_TBVeiculo_TBAgrupamento] FOREIGN KEY ([Id]) REFERENCES [dbo].[TBAgrupamento] ([Id])
 );
+
+
+GO
+ALTER TABLE [dbo].[TBVeiculo] NOCHECK CONSTRAINT [FK_TBVeiculo_TBAgrupamento];
 
