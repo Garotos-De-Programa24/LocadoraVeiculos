274edd558f1f71694fd676b9363d45d9ab791c00
nicolas <nicolas56mil@gmail.com>
2022-07-26 16:55:15 -0300

    agrupamentooo

---
+++ LocadoraVeiculos.Infra.ORM/ModuloAgrupamento/MapeadorAgrupamentoORM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LocadoraVeiculos.Dominio.ModuloAgrupamento;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LocadoraVeiculos.Infra.ORM.ModuloAgrupamento
+{
+    public class MapeadorAgrupamento : IEntityTypeConfiguration<Agrupamento>
+    {
+        public void Configure(EntityTypeBuilder<Agrupamento> builder)
+        {
+            builder.ToTable("TBAgrupamento");
+            builder.Property(x => x.Id).ValueGeneratedNever();
+            builder.Property(x => x.Nome).HasColumnType("varchar(100)").IsRequired();
+
+        }
+
+    }
+}
---
+++ LocadoraVeiculos.Infra.ORM/ModuloAgrupamento/RepositorioAgrupamentoORM.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LocadoraVeiculos.Dominio.ModuloAgrupamento;
+using LocadoraVeiculos.Infra.ORM.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LocadoraVeiculos.Infra.ORM.ModuloAgrupamento
+{
+    public class RepositorioAgrupamentoORM : IRepositorioAgrupamento
+    {
+        private DbSet<Agrupamento> agrupamentos;
+        private readonly LocadoraVeiculoDbContext dbContext;
+
+        public RepositorioAgrupamentoORM(LocadoraVeiculoDbContext dbContext)
+        {
+            agrupamentos = dbContext.Set<Agrupamento>();
+            this.dbContext = dbContext;
+        }
+        public void Inserir(Agrupamento novoRegistro)
+        {
+            agrupamentos.Add(novoRegistro);
+        }
+        public void Editar(Agrupamento registro)
+        {
+            agrupamentos.Update(registro);
+        }
+
+        public void Excluir(Agrupamento registro)
+        {
+            agrupamentos.Remove(registro);
+        }
+
+        public Agrupamento SelecionarPorId(Guid id)
+        {
+            return agrupamentos.SingleOrDefault(x => x.Id == id);
+        }
+
+        public Agrupamento SelecionarAgrupamentoPorNome(string nome)
+        {
+            return agrupamentos.SingleOrDefault(x => x.Nome == nome);
+        }
+        public List<Agrupamento> SelecionarTodos()
+        {
+            return agrupamentos.ToList();
+        }
+    }
+}
