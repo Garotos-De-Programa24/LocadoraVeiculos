8b580a9fc9601c16ffdd92a57a9bfb96b8795156
nicolas <nicolas56mil@gmail.com>
2022-07-26 17:09:16 -0300

    taxassss

---
+++ LocadoraVeiculos.Infra.ORM/ModuloTaxa/MapeadorTaxaORM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LocadoraVeiculos.Dominio.ModuloTaxa;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LocadoraVeiculos.Infra.ORM.ModuloTaxa
+{
+    public class RepositorioTaxaORM : IEntityTypeConfiguration<Taxa>
+    {
+        public void Configure(EntityTypeBuilder<Taxa> builder)
+        {
+            builder.ToTable("TBTaxa");
+            builder.Property(x => x.Id).ValueGeneratedNever();
+            builder.Property(x => x.Equipamento).HasColumnType("varchar(200)").IsRequired();
+            builder.Property(x => x.Valor).HasColumnType("varchar(40)").IsRequired();
+            builder.Property(x => x.TaxaDiaria).HasColumnType("varchar(200)").IsRequired();
+        }
+    }
+}
---
+++ LocadoraVeiculos.Infra.ORM/ModuloTaxa/RepositorioTaxaORM.cs
@@ -0,0 +1,50 @@
+﻿using LocadoraVeiculos.Dominio.Compartilhado;
+using LocadoraVeiculos.Dominio.ModuloCliente;
+using LocadoraVeiculos.Dominio.ModuloTaxa;
+using LocadoraVeiculos.Infra.ORM.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LocadoraVeiculos.Infra.ORM.ModuloCliente
+{
+    public class RepositorioTaxaORM : IRepositorioTaxa
+    {
+        private DbSet<Taxa> taxas;
+        private readonly LocadoraVeiculoDbContext dbContext;
+
+        public RepositorioTaxaORM(LocadoraVeiculoDbContext dbContext)
+        {
+            taxas = dbContext.Set<Taxa>();
+            this.dbContext = dbContext;
+        }
+        public void Inserir(Taxa novoRegistro)
+        {
+            taxas.Add(novoRegistro);
+        }
+        public void Editar(Taxa registro)
+        {
+            taxas.Update(registro);
+        }
+
+        public void Excluir(Taxa registro)
+        {
+            taxas.Remove(registro);
+        }
+
+        public Taxa SelecionarPorId(Guid id)
+        {
+            return taxas.SingleOrDefault(x => x.Id == id);
+        }
+
+        public Taxa SelecionarTaxaPeloNome(string nome)
+        {
+            return taxas.SingleOrDefault(x => x.Equipamento == nome);
+        }
+        public List<Taxa> SelecionarTodos()
+        {
+            return taxas.ToList();
+        }
+    }
+}
