57ab754d1ba00f246c801ed052dea9d4dd801c83
AnderSpidy <andersonreinaldo2347@gmail.com>
2022-07-26 16:24:49 -0300

    ORM TbFuncionario

---
+++ LocadoraVeiculos.Infra.ORM/ModuloFuncionario/MapeadorFuncionarioORM.cs
@@ -0,0 +1,26 @@
+﻿using LocadoraVeiculos.Dominio.ModuloFuncionario;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraVeiculos.Infra.ORM.ModuloFuncionario
+{
+    public class MapeadorFuncionarioORM : IEntityTypeConfiguration<Funcionario>
+    {
+        public void Configure(EntityTypeBuilder<Funcionario> builder)
+        {
+            builder.ToTable("TbFuncionario");
+            builder.Property(x => x.Id).ValueGeneratedNever();
+            builder.Property(x => x.Nome).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(x => x.Salario).HasColumnType("varchar(20)").IsRequired();
+            builder.Property(x => x.DataAdmissao).IsRequired();
+            builder.Property(x => x.Login).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(x => x.Senha).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(x => x.Gerente);
+        }
+    }
+}
---
+++ LocadoraVeiculos.Infra.ORM/ModuloFuncionario/RepositorioFuncionarioORM.cs
@@ -0,0 +1,58 @@
+﻿using LocadoraVeiculos.Dominio.ModuloFuncionario;
+using LocadoraVeiculos.Infra.ORM.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraVeiculos.Infra.ORM.ModuloFuncionario
+{
+    public class RepositorioFuncionarioORM : IRepositorioFuncionario
+    {
+        private DbSet<Funcionario> funcionarios;
+        private readonly LocadoraVeiculoDbContext dbContext;
+
+        public RepositorioFuncionarioORM(LocadoraVeiculoDbContext dbContext)
+        {
+            funcionarios = dbContext.Set<Funcionario>();
+            this.dbContext = dbContext;
+        }
+        public void Inserir(Funcionario novoRegistro)
+        {
+            funcionarios.Add(novoRegistro);
+        }
+        public void Editar(Funcionario registro)
+        {
+            funcionarios.Update(registro);
+        }
+
+        public void Excluir(Funcionario registro)
+        {
+            funcionarios.Remove(registro);
+        }
+
+        
+
+        public Funcionario SelecionarFuncionarioPorNome(string nome)
+        {
+            return funcionarios.SingleOrDefault(x => x.Nome == nome);
+        }
+
+        public Funcionario SelecionarFuncionarioPorUsuario(string usuario)
+        {
+            return funcionarios.SingleOrDefault(x => x.Login == usuario);
+        }
+
+        public Funcionario SelecionarPorId(Guid id)
+        {
+            return funcionarios.SingleOrDefault(x => x.Id == id);
+        }
+
+        public List<Funcionario> SelecionarTodos()
+        {
+            return funcionarios.ToList();
+        }
+    }
+}
